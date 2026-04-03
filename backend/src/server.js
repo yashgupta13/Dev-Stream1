6941b108ae95606eff1f4f4d295bc291eb306596
Yash <yashgupta5809@gmail.com>
2026-04-03 15:13:48 +0530

    fixed vercel routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.get("/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
